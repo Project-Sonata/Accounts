6e98732a92cff5221d07b52780b3acafd722ebc2
odeyalo <theprogrammerinfuture@gmail.com>
2023-07-06 21:03:02 +0300

    Added tests

--- src/test/java/com/odeyalo/sonata/account/controller/AccountControllerTest.java
+++ src/test/java/com/odeyalo/sonata/account/controller/AccountControllerTest.java
@@ -1,5 +1,6 @@
 package com.odeyalo.sonata.account.controller;
 
+import com.odeyalo.sonata.account.SharedComponent;
 import com.odeyalo.sonata.account.dto.AccountInformationDto;
 import com.odeyalo.sonata.account.entity.Account;
 import com.odeyalo.sonata.account.entity.Gender;
@@ -13,8 +14,9 @@ import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRun
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Hooks;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -30,7 +32,10 @@ import static org.springframework.cloud.contract.stubrunner.spring.StubRunnerPro
         repositoryRoot = "git://https://github.com/Project-Sonata/Sonata-Contracts.git",
         ids = "com.odeyalo.sonata:authorization:*")
 @Import(SharedComponent.class)
+@TestPropertySource(locations = "classpath:application-test.properties")
 class AccountControllerTest {
+
+    public static final String CURRENT_ACCOUNT_ENDPOINT = "/account/me";
 
     @Autowired
     WebTestClient webClient;
@@ -40,8 +45,11 @@ class AccountControllerTest {
 
     Account expectedAccount;
 
+    String prefix = "Bearer ";
+
     @BeforeAll
     void setUp() {
+        Hooks.onOperatorDebug();
         PersistableAccount account = PersistableAccount.builder()
                 .id("1")
                 .email("nakanomiku@gmail.com")
@@ -51,9 +59,8 @@ class AccountControllerTest {
                 .creationTime(System.currentTimeMillis())
                 .birthdate(LocalDate.of(2004, Month.MAY, 5))
                 .build();
-        expectedAccount = accountStorage.save(account).block();
 
-        System.out.println(expectedAccount);
+        expectedAccount = accountStorage.save(account).block();
     }
 
     //    GET /account/me - get the account info for current user.
@@ -78,7 +85,33 @@ class AccountControllerTest {
 
         private WebTestClient.ResponseSpec prepareRequestWithoutAuthorizationAndSend() {
             return webClient.get()
-                    .uri("/account/me")
+                    .uri(CURRENT_ACCOUNT_ENDPOINT)
+                    .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .exchange();
+        }
+    }
+
+    @Nested
+    @Import(SharedComponent.class)
+    class CurrentAccountEndpointWithInvalidToken {
+        String invalidTokenValue = "invalid_token";
+
+        @Test
+        void expect401() {
+            WebTestClient.ResponseSpec responseSpec = prepareRequestWithInvalidAuthorizationAndSend();
+            responseSpec.expectStatus().isUnauthorized();
+        }
+
+        @Test
+        void expectApplicationJson() {
+            WebTestClient.ResponseSpec responseSpec = prepareRequestWithInvalidAuthorizationAndSend();
+            responseSpec.expectHeader().contentType(MediaType.APPLICATION_JSON);
+        }
+
+        private WebTestClient.ResponseSpec prepareRequestWithInvalidAuthorizationAndSend() {
+            return webClient.get()
+                    .uri(CURRENT_ACCOUNT_ENDPOINT)
+                    .header(HttpHeaders.AUTHORIZATION, invalidTokenValue)
                     .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                     .exchange();
         }
@@ -87,7 +120,7 @@ class AccountControllerTest {
     @Nested
     @Import(SharedComponent.class)
     class CurrentAccountEndpointWithValidAuthorization {
-        String authorizationHeaderValue = "mikunakanoisthebestgirl";
+        String authorizationHeaderValue = prefix + "mikunakanoisthebestgirl";
 
         @Test
         @DisplayName("Expect 200 OK status code")
@@ -162,7 +195,7 @@ class AccountControllerTest {
 
         private WebTestClient.ResponseSpec prepareRequestWithAuthorizationHeaderAndSend() {
             return webClient.get()
-                    .uri("/account/me")
+                    .uri(CURRENT_ACCOUNT_ENDPOINT)
                     .header(HttpHeaders.AUTHORIZATION, authorizationHeaderValue)
                     .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                     .exchange();
@@ -170,6 +203,3 @@ class AccountControllerTest {
     }
 }
 
-@Component
-class SharedComponent {
-}
