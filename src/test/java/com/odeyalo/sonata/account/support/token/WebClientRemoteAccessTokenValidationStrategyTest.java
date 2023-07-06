fabc0986d6e35273c9524a3901e16a971c0c24b0
odeyalo <theprogrammerinfuture@gmail.com>
2023-07-06 21:01:41 +0300

    Added test configuration

--- src/test/java/com/odeyalo/sonata/account/support/token/WebClientRemoteAccessTokenValidationStrategyTest.java
+++ src/test/java/com/odeyalo/sonata/account/support/token/WebClientRemoteAccessTokenValidationStrategyTest.java
@@ -1,22 +1,38 @@
 package com.odeyalo.sonata.account.support.token;
 
+import com.odeyalo.sonata.account.configuration.WebClientAccessTokenValidationStrategyConfiguration;
+import com.odeyalo.sonata.account.support.token.converter.TokenIntrospectionResponse2ValidatedAccessTokenConverter;
+import com.odeyalo.sonata.account.support.token.converter.ValidatedAccessTokenConverter;
+import com.odeyalo.sonata.common.authorization.TokenIntrospectionResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties.StubsMode.REMOTE;
 
-@SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Import(WebClientRemoteAccessTokenValidationStrategyTestConfiguration.class)
 @AutoConfigureStubRunner(stubsMode = REMOTE,
         repositoryRoot = "git://https://github.com/Project-Sonata/Sonata-Contracts.git",
         ids = "com.odeyalo.sonata:authorization:+:stubs:${stubrunner.runningstubs.authorization.port}")
 @TestPropertySource(locations = "classpath:application-test.properties")
+@AutoConfigureWebClient
+@ExtendWith(SpringExtension.class)
 class WebClientRemoteAccessTokenValidationStrategyTest {
+
 
     @Autowired
     WebClientRemoteAccessTokenValidationStrategy validationStrategy;
@@ -52,5 +68,28 @@ class WebClientRemoteAccessTokenValidationStrategyTest {
     void validateInvalidAccessToken_andExpectNull() {
         ValidatedAccessToken token = validationStrategy.validateAccessToken("invalidtoken").block();
         assertEquals(ValidatedAccessToken.invalid(), token, "Token must be equal to invalid one!");
+    }
+}
+
+@TestConfiguration
+class WebClientRemoteAccessTokenValidationStrategyTestConfiguration {
+
+    @Bean
+    public ValidatedAccessTokenConverter<TokenIntrospectionResponse> validatedAccessTokenConverter() {
+        return new TokenIntrospectionResponse2ValidatedAccessTokenConverter();
+    }
+
+    @Bean
+    public WebClientRemoteAccessTokenValidationStrategy webClientRemoteAccessTokenValidationStrategy(
+            @Qualifier("tokenValidationWebClient") WebClient tokenValidationWebClient,
+            ValidatedAccessTokenConverter<TokenIntrospectionResponse> accessTokenConverter) {
+        return new WebClientRemoteAccessTokenValidationStrategy(tokenValidationWebClient, accessTokenConverter);
+    }
+
+    @Bean
+    public WebClient tokenValidationWebClient(@Value("${sonata.token.validation.remote.webclient.url}") String url, WebClient.Builder builder) {
+        return builder
+                .baseUrl(url)
+                .build();
     }
 }
