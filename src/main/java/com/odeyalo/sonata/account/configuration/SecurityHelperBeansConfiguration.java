46c51d6dd57cf3b9ceaecb3b854b97abc29b8754
odeyalo <theprogrammerinfuture@gmail.com>
2023-07-07 17:54:24 +0300

    Added ServerAuthenticationFailureHandler

--- src/main/java/com/odeyalo/sonata/account/configuration/SecurityHelperBeansConfiguration.java
+++ src/main/java/com/odeyalo/sonata/account/configuration/SecurityHelperBeansConfiguration.java
@@ -7,10 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
 import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
+import org.springframework.security.web.server.authentication.ServerAuthenticationEntryPointFailureHandler;
+import org.springframework.security.web.server.authentication.ServerAuthenticationFailureHandler;
 
 @Configuration
 public class SecurityHelperBeansConfiguration {
@@ -18,7 +19,10 @@ public class SecurityHelperBeansConfiguration {
     @Autowired
     ReactiveAuthenticationManager manager;
 
-
+    @Bean
+    public ServerAuthenticationFailureHandler serverAuthenticationFailureHandler(ServerAuthenticationEntryPoint entryPoint) {
+        return new ServerAuthenticationEntryPointFailureHandler(entryPoint);
+    }
 
     @Bean
     public TokenServerAuthenticationConverter tokenServerAuthenticationConverter(AccessTokenExtractor accessTokenExtractor) {
@@ -26,7 +30,8 @@ public class SecurityHelperBeansConfiguration {
     }
 
     @Bean
-    public AuthenticationWebFilter authenticationWebFilter(ServerAuthenticationConverter authenticationConverter) {
-        return new UserAuthenticationWebFilter(authenticationConverter, manager);
+    public AuthenticationWebFilter authenticationWebFilter(ServerAuthenticationConverter authenticationConverter,
+                                                           ServerAuthenticationFailureHandler failureHandler) {
+        return new UserAuthenticationWebFilter(authenticationConverter, manager, failureHandler);
     }
 }
