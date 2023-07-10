8f5eb08185cd1670a7a0cf7d6b38da082ecb570f
odeyalo <theprogrammerinfuture@gmail.com>
2023-07-10 18:11:05 +0300

    Replaced stub with real object, it will be refactored

--- src/main/java/com/odeyalo/sonata/account/controller/AccountController.java
+++ src/main/java/com/odeyalo/sonata/account/controller/AccountController.java
@@ -1,9 +1,13 @@
 package com.odeyalo.sonata.account.controller;
 
 import com.odeyalo.sonata.account.dto.AccountInformationDto;
+import com.odeyalo.sonata.account.repository.storage.PersistableAccount;
 import com.odeyalo.sonata.account.repository.storage.ReactiveAccountStorage;
+import com.odeyalo.sonata.account.web.security.auth.AuthenticatedSubject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,15 +22,21 @@ public class AccountController {
 
     @GetMapping(value = "/me", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<AccountInformationDto> getCurrentAccountInfo() {
-        return accountStorage.findById("1")
-                .map(account -> AccountInformationDto.builder()
-                        .id(account.getId())
-                        .username(account.getUsername())
-                        .email(account.getEmail())
-                        .gender(account.getGender().toString())
-                        .birthdate(account.getBirthdate())
-                        .countryCode(account.getCountryCode())
-                        .build());
+        return ReactiveSecurityContextHolder.getContext()
+                .map(SecurityContext::getAuthentication)
+                .map(authentication -> ((AuthenticatedSubject) authentication).getDetails().getId())
+                .flatMap(id -> accountStorage.findById(id))
+                .map(AccountController::convertToDto);
     }
 
+    private static AccountInformationDto convertToDto(PersistableAccount account) {
+        return AccountInformationDto.builder()
+                .id(account.getId())
+                .username(account.getUsername())
+                .email(account.getEmail())
+                .gender(account.getGender().toString())
+                .birthdate(account.getBirthdate())
+                .countryCode(account.getCountryCode())
+                .build();
+    }
 }
