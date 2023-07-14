9222b4104c118e00e876d1ad280b375a03ecbe3e
odeyalo <theprogrammerinfuture@gmail.com>
2023-07-14 22:22:04 +0300

    Interface to update the user's password if and only if provided old password is valid

---
+++ src/main/java/com/odeyalo/sonata/account/service/password/SecurePasswordUpdater.java
@@ -0,0 +1,17 @@
+package com.odeyalo.sonata.account.service.password;
+
+import com.odeyalo.sonata.account.web.security.auth.AuthenticatedUser;
+import reactor.core.publisher.Mono;
+
+/**
+ * Interface to update the user's password if and only if provided old password is valid
+ */
+public interface SecurePasswordUpdater {
+    /**
+     * Update the user's password if and only if {@link PasswordContainer#getOldPassword} matches old user's password
+     * @param user - user to update password to
+     * @param container - container with passwords
+     * @return - result of the password updating
+     */
+    Mono<PasswordUpdatingResult> updatePassword(AuthenticatedUser user, PasswordContainer container);
+}
